9ecdfb75a1a00b3c3ade621c885c59e38b05649c
rogersgt <gradytrogers@gmail.com>
2019-01-23 22:50:11 -0500

    stringify response body

--- src/responses/success.js
+++ src/responses/success.js
@@ -1,7 +1,7 @@
 export default function success(data) {
   return {
     statusCode: !!data ? 200 : 204,
-    body: data,
+    body: JSON.stringify(data) || data,
     headers: {
       'Access-Control-Allow-Origin': process.env.APP_ORIGIN,
       'Access-Control-Allow-Credentials': '*'
